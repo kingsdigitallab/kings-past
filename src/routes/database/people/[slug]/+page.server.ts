8974da55146a0bd6b862e3f368700a2654f88031
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-04-23 15:57:30 +0100

    feat(people): display relationship name

--- src/routes/database/people/[slug]/+page.server.ts
+++ src/routes/database/people/[slug]/+page.server.ts
@@ -8,7 +8,8 @@ export async function load({ params, parent }) {
 		const knows = await supabase
 			.from('person_knows')
 			.select('')
-			.or(`person.eq.${params.slug},knows.eq.${params.slug}`);
+			.or(`person.eq.${params.slug},knows.eq.${params.slug}`)
+			.order('knows,person');
 
 		const parentData = await parent();
 
--- src/routes/database/people/[slug]/+page.svelte
+++ src/routes/database/people/[slug]/+page.svelte
@@ -1,47 +1,59 @@
 <script lang="ts">
-  export let data;
+	export let data;
 </script>
 
 <article>
-  <h1>{data.person.name}</h1>
+	<h1>{data.person.name}</h1>
 
-  <section>
-    <h2>Metadata</h2>
-    <dl>
-      <dt>Alternative names</dt>
-      <dd>
-        {#if data.person.alternative_names}{data.person.alternative_names}{/if}
-      </dd>
-      <dt>Gender</dt>
-      <dd>{data.person.gender}</dd>
-      <dt>Nationality</dt>
-      <dd>{data.person.nationality}</dd>
-      <dt>Ethnicity</dt>
-      <dd>{data.person.ethnicity}</dd>
-      <dt>Languages</dt>
-      <dd>{data.person.languages}</dd>
-    </dl>
-  </section>
+	<section>
+		<h2>Metadata</h2>
+		<dl>
+			<dt>Alternative names</dt>
+			<dd>
+				{data.person.alternative_names || null}
+			</dd>
+			<dt>Gender</dt>
+			<dd>{data.person.gender}</dd>
+			<dt>Nationality</dt>
+			<dd>{data.person.nationality}</dd>
+			<dt>Ethnicity</dt>
+			<dd>{data.person.ethnicity}</dd>
+			<dt>Languages</dt>
+			<dd>{data.person.languages || null}</dd>
+		</dl>
+	</section>
 
-  {#if data.knows}
-    <section>
-      <h2>Knows</h2>
-      <ul>
-        {#each data.knows as knows}
-          {#if knows.person === data.person.slug}
-            <li><a href={knows.knows}>{data.people[knows.knows].name}</a></li>
-          {:else}
-            <li><a href={knows.person}>{data.people[knows.person].name}</a></li>
-          {/if}
-        {/each}
-      </ul>
-    </section>
-  {/if}
+	{#if data.knows}
+		<section>
+			<h2>Knows</h2>
+			<ul>
+				{#each data.knows as knows}
+					<li>
+						{#if knows.person === data.person.slug}
+							<a href={knows.knows}>{data.people[knows.knows].name}</a>
+						{:else}
+							<a href={knows.person}>{data.people[knows.person].name}</a>
+						{/if}
+						{#if knows.relationship}
+							<span>, {knows.relationship.toLowerCase()} relationship</span>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</section>
+	{/if}
 
-  {#if data.person.description}
-    <article>
-      <h2>About</h2>
-      <p>{data.person.description}</p>
-    </article>
-  {/if}
+	{#if data.person.description}
+		<article>
+			<h2>About</h2>
+			<p>{data.person.description}</p>
+		</article>
+	{/if}
 </article>
+
+<style>
+	li {
+		display: flex;
+		align-items: baseline;
+	}
+</style>
