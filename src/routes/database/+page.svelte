219ce38217b9db9151716145381cee989910fc5e
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-04 17:51:04 +0100

    fix(database): remove element that was not needed

--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-  export let data;
+	export let data;
 </script>
 
 <article>
-  <header><h1>Database</h1></header>
+	<h1>Database</h1>
 
-  <ul>
-    <li><a href="{data.url}/people">People</a></li>
-    <li><a href="{data.url}/organisations">Organisations</a></li>
-    <li><a href="{data.url}/places">Places</a></li>
-    <li><a href="{data.url}/events">Events</a></li>
-    <li><a href="{data.url}/donations">Donations</a></li>
-  </ul>
+	<ul>
+		<li><a href="{data.url}/people">People</a></li>
+		<li><a href="{data.url}/organisations">Organisations</a></li>
+		<li><a href="{data.url}/places">Places</a></li>
+		<li><a href="{data.url}/events">Events</a></li>
+		<li><a href="{data.url}/donations">Donations</a></li>
+	</ul>
 </article>
