083c4a931dd6fb973d8c8d41308c20d43eacfc85
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-04 16:47:14 +0100

    style: format code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import type { Moment } from '$lib/types';
 import { base } from '$app/paths';
 
-
 export async function load({ fetch }) {
 	const response = await fetch(`${base}/api/moments`);
 	const moments: Moment[] = await response.json();
