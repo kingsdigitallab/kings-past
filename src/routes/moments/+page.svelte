eb105358fbb4209654c7db4b32de2130ee910f22
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-04 17:52:01 +0100

    feat(moments): add basic styling to the index page

--- src/routes/moments/+page.svelte
+++ src/routes/moments/+page.svelte
@@ -6,9 +6,23 @@
 	<h1>Moments</h1>
 
 	{#each data.moments as moment}
-		<article>
+		<article class="surface-1">
 			<h2><a href="moments/{moment.slug}">{moment.title}</a></h2>
 			{moment.excerpt}
 		</article>
 	{/each}
 </article>
+
+<style>
+	article {
+		margin-bottom: 2rem;
+
+		& h2 {
+			max-inline-size: none;
+		}
+
+		&:is(a:hover) {
+			text-decoration: none;
+		}
+	}
+</style>
