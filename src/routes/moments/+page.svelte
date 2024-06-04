be58fc6dfd161915ba59d96d3b635d9330e23ab0
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-04 16:47:37 +0100

    feat(moments): add moments index page
    This is not styled yet

--- src/routes/moments/+page.svelte
+++ src/routes/moments/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Moments</h1>
+<script lang="ts">
+	export let data;
+</script>
+
+<article>
+	<h1>Moments</h1>
+
+	{#each data.moments as moment}
+		<article>
+			<h2><a href="moments/{moment.slug}">{moment.title}</a></h2>
+			{moment.excerpt}
+		</article>
+	{/each}
+</article>
