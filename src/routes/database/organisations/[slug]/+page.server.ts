5cb83173cc7c420f2103dfdaaf3f5abcf992fba0
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-27 14:50:38 +0100

    feat(organisations): use the entity component to render the organisation

--- src/routes/database/organisations/[slug]/+page.server.ts
+++ src/routes/database/organisations/[slug]/+page.server.ts
@@ -1,12 +1,49 @@
-import { getRecord } from '$lib/supabase';
+import { EMPTY_PLACEHOLDER } from '$lib/constants';
+import { getRecord, getRecords, supabase } from '$lib/supabase';
 import { error } from '@sveltejs/kit';
+import { compile } from 'mdsvex';
 
 export async function load({ params }) {
 	try {
+		const item = await getRecord('organisation', params.slug);
+
+		const meta = {
+			'Alternative names': item.alternative_names || EMPTY_PLACEHOLDER,
+			'Founding date': item.founding_date || EMPTY_PLACEHOLDER,
+			'Dissolution date': item.dissolution_date || EMPTY_PLACEHOLDER,
+			Location: item.location || EMPTY_PLACEHOLDER,
+			'Organisation type': item.organisation_type || EMPTY_PLACEHOLDER
+		};
+
+		const description = await compile(item.description);
+
+		const members = await supabase
+			.from('person_member_of')
+			.select('')
+			.eq('organisation', params.slug)
+			.order('person');
+
+		const sources = await supabase
+			.from('person_source')
+			.select('')
+			.eq('person', params.slug)
+			.order('source');
+
+		const people = await getRecords('person');
+
 		return {
-			item: await getRecord('organisation', params.slug)
+			item,
+			meta,
+			description,
+			members: members.data,
+			sources,
+			people: people?.reduce((acc, cur) => {
+				acc[cur.slug] = cur;
+				return acc;
+			}, {})
 		};
 	} catch (e) {
+		console.error(e);
 		error(404, `Could not find ${params.slug}`);
 	}
 }
--- src/routes/database/organisations/[slug]/+page.svelte
+++ src/routes/database/organisations/[slug]/+page.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-  export let data;
+	import Entity from '$lib/components/entity.svelte';
+
+	export let data;
 </script>
 
-<article>
-  <h1>{data.item.name}</h1>
-  {#if data.item.description}
-    <p>{data.item.description}</p>
-  {/if}
-</article>
+<Entity
+	entityType="Organisation"
+	entityName={data.item.name}
+	meta={data.meta}
+	description={data.description?.code}
+	sources={data.sources}
+>
+	{#if data.members}
+		<section>
+			<h2>Members</h2>
+			<ul>
+				{#each data.members as member}
+					<li>
+						<a href="../people/{member.person}">{data.people[member.person].name}</a>
+					</li>
+				{/each}
+			</ul>
+		</section>
+	{/if}
+</Entity>
