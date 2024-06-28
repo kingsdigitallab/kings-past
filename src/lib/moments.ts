7e53e6e74595371f1fd3f5f2480b5f2f07c7bb2f
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-28 10:06:45 +0100

    feat(moments): add function to get detailed research related to a moment

--- src/lib/moments.ts
+++ src/lib/moments.ts
@@ -22,3 +22,30 @@ export async function getMoments() {
 export function getMomentN(n: number | string) {
 	return String(n).padStart(2, '0');
 }
+
+export async function getResearch(moment: string) {
+	const essays = [];
+
+	const paths = import.meta.glob(['/src/moments/*/*.md', '!/src/moments/*/index.md'], {
+		eager: true
+	});
+
+	for (const path in paths) {
+		const file = paths[path];
+		const slug = path.split('/').at(-1)?.split('.').at(0);
+
+		if (
+			path.startsWith(`/src/moments/${moment}`) &&
+			file &&
+			typeof file === 'object' &&
+			'metadata' in file &&
+			slug
+		) {
+			const metadata = file.metadata as Omit<Moment, 'slug'>;
+			const essay = { ...metadata, slug };
+			essays.push(essay);
+		}
+	}
+
+	return essays;
+}
