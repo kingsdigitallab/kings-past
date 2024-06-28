3aca4e16141fb44aed35cf99edcdcd9e62a978b1
jmiguelv <jmiguelv@users.noreply.github.com>
2024-06-28 13:35:59 +0000

    deploy: 64f9d12b1ecb3ab28ee9b35c20e218e817c2782b

--- _app/immutable/chunks/paths.DOO6bVnC.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1q7yyer)==null?void 0:s.base)??"/kings-past";var e;const t=((e=globalThis.__sveltekit_1q7yyer)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/entry/start.BhAV8B89.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.C6ZYsL7D.js";export{t as start};
