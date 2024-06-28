d0b685fa8e4cfea4dfaec03d79a0f0a1e0bfbcff
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-28 11:21:29 +0100

    feat(moment): add style to the research and lives sections

--- src/routes/moments/[slug]/+page.svelte
+++ src/routes/moments/[slug]/+page.svelte
@@ -5,27 +5,47 @@
 </script>
 
 <Moment meta={data.meta} content={data.content} moments={data.moments}>
-	{#if data.essays && data.essays.length > 0}
-		<section>
-			<h2>Detailed research</h2>
-			<ul>
-				{#each data.essays as essay}
-					<li><a href="{data.slug}/{essay.slug}">{essay.title}</a></li>
-				{/each}
-			</ul>
-		</section>
-	{/if}
+	{#if (data.essays && data.essays.length > 0) || (data.momentPeople && data.momentPeople.length > 0)}
+		<section class="links">
+			{#if data.essays && data.essays.length > 0}
+				<section class="surface-4">
+					<h2>Detailed research</h2>
+					<ul>
+						{#each data.essays as essay}
+							<li><a href="{data.slug}/{essay.slug}">{essay.title}</a></li>
+						{/each}
+					</ul>
+				</section>
+			{/if}
 
-	{#if data.momentPeople && data.momentPeople.length > 0}
-		<section>
-			<h2>King's lives</h2>
-			<ul>
-				{#each data.momentPeople as person}
-					<li>
-						<a href="../database/people/{person.person}">{data.people[person.person].name}</a>
-					</li>
-				{/each}
-			</ul>
+			{#if data.momentPeople && data.momentPeople.length > 0}
+				<section class="surface-4">
+					<h2>King's lives</h2>
+					<ul>
+						{#each data.momentPeople as person}
+							<li>
+								<a href="../database/people/{person.person}">{data.people[person.person].name}</a>
+							</li>
+						{/each}
+					</ul>
+				</section>
+			{/if}
 		</section>
 	{/if}
 </Moment>
+
+<style>
+	.links {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(var(--size-14), 1fr));
+		grid-gap: var(--size-8);
+
+		& section {
+			padding-inline: var(--size-4);
+		}
+	}
+
+	li {
+		font-size: var(--font-size-3);
+	}
+</style>
