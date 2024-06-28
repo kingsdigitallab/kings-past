2fc4e1a5016bde06ddfd6dab410fff993d214bb8
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-28 10:07:23 +0100

    feat(moments): render detailed research in the moment page

--- src/routes/moments/[slug]/+layout.ts
+++ src/routes/moments/[slug]/+layout.ts
@@ -1,16 +1,19 @@
 import { error } from '@sveltejs/kit';
-import { getMoments } from '$lib/moments.js';
+import { getMomentN, getMoments, getResearch } from '$lib/moments';
 
 export async function load({ data, params }) {
 	try {
 		const moment = await import(`../../../moments/${params.slug}/index.md`);
 
 		const moments = await getMoments();
+		const essays = await getResearch(params.slug);
 
 		return {
+			slug: getMomentN(moment.metadata.n),
 			meta: moment.metadata,
 			content: moment.default,
-			moments: moments,
+			moments,
+			essays,
 			momentPeople: data.momentPeople,
 			people: data.people
 		};
--- src/routes/moments/[slug]/+page.svelte
+++ src/routes/moments/[slug]/+page.svelte
@@ -5,6 +5,17 @@
 </script>
 
 <Moment meta={data.meta} content={data.content} moments={data.moments}>
+	{#if data.essays && data.essays.length > 0}
+		<section>
+			<h2>Detailed research</h2>
+			<ul>
+				{#each data.essays as essay}
+					<li><a href="{data.slug}/{essay.slug}">{essay.title}</a></li>
+				{/each}
+			</ul>
+		</section>
+	{/if}
+
 	{#if data.momentPeople && data.momentPeople.length > 0}
 		<section>
 			<h2>King's lives</h2>
