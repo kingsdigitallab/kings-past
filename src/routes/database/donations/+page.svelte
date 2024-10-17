bc89b30f180d2a5e0fac308b3a020b76bb630841
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-17 18:23:48 +0100

    feat(donations): use the indextable plugin

--- src/routes/database/donations/+page.svelte
+++ src/routes/database/donations/+page.svelte
@@ -1,40 +1,64 @@
 <script lang="ts">
 	import * as config from '$lib/config';
-	import { createTable, Render, Subscribe } from 'svelte-headless-table';
-	import { addPagination } from 'svelte-headless-table/plugins';
-	import { readable } from 'svelte/store';
+	import IndexTable from '$lib/components/IndexTable.svelte';
 
 	export let data;
 
-	const { collection } = data;
+	const { collection, url } = data;
+	const label = 'donations';
 
-	const table = createTable(readable(collection), {
-		pagination: addPagination({ initialPageSize: 5 })
-	});
-	const columns = table.createColumns([
-		table.column({
-			header: 'Start date',
-			accessor: 'start_date'
-		}),
-		table.column({
-			header: 'End date',
-			accessor: 'end_date'
-		}),
-		table.column({
+	const columns = [
+		{
+			header: 'Date',
+			id: 'date',
+			accessor: (item) => ({ start_date: item.start_date, end_date: item.end_date }),
+			cell: ({ value }) => {
+				const { start_date, end_date } = value;
+				return start_date !== end_date ? `${start_date} - ${end_date}` : start_date;
+			},
+			plugins: {
+				sort: {
+					getSortValue(item) {
+						return [item.start_date, item.end_date];
+					}
+				}
+			}
+		},
+		{
 			header: 'Name',
+			id: 'name',
 			accessor: (item) => ({ slug: item.slug, name: item.name }),
-			id: 'name'
-		}),
-		table.column({
-			header: 'Description',
-			accessor: 'description',
-			cell: ({ value }) => value?.slice(0, 50) + '…'
-		})
-	]);
-
-	const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates } =
-		table.createViewModel(columns);
-	const { pageIndex, pageCount, pageSize, hasNextPage, hasPreviousPage } = pluginStates.pagination;
+			plugins: {
+				sort: {
+					getSortValue(item: { slug: string; name: string }) {
+						return item.slug;
+					}
+				}
+			}
+		},
+		{
+			header: 'Location',
+			accessor: 'location',
+			cell: ({ value }) =>
+				value
+					?.split('-')
+					.slice(0, -1)
+					.map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+					.join(' ') || 'N/A',
+			plugins: {
+				sort: {
+					getSortValue(item: string) {
+						return item ?? '';
+					}
+				}
+			}
+		},
+		{
+			header: 'Price',
+			accessor: 'price',
+			cell: ({ value, row }) => `${value.toLocaleString()} ${row.original.currency}`
+		}
+	];
 </script>
 
 <svelte:head>
@@ -46,94 +70,5 @@
 		<h1>Donations</h1>
 	</header>
 
-	<section>
-		<h2>
-			Displaying {$pageIndex * $pageSize + 1} to {Math.min(
-				($pageIndex + 1) * $pageSize,
-				collection.length
-			)} of {collection.length}
-			donations
-		</h2>
-		<table {...$tableAttrs}>
-			<thead>
-				{#each $headerRows as headerRow (headerRow.id)}
-					<Subscribe rowAttrs={headerRow.attrs()} let:rowAttrs>
-						<tr {...rowAttrs}>
-							{#each headerRow.cells as cell (cell.id)}
-								<Subscribe attrs={cell.attrs()} let:attrs>
-									<th class="surface-2" {...attrs}>
-										<Render of={cell.render()} />
-									</th>
-								</Subscribe>
-							{/each}
-						</tr>
-					</Subscribe>
-				{/each}
-			</thead>
-			<tbody {...$tableBodyAttrs}>
-				{#each $pageRows as row (row.id)}
-					<Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-						<tr {...rowAttrs}>
-							{#each row.cells as cell (cell.id)}
-								<Subscribe attrs={cell.attrs()} let:attrs>
-									<td class={cell.id} {...attrs}>
-										{#if cell.id === 'name'}
-											<a href={`${data.url}/${cell.value.slug}`}>{cell.value.name}</a>
-										{:else}
-											<Render of={cell.render()} />
-										{/if}
-									</td>
-								</Subscribe>
-							{/each}
-						</tr>
-					</Subscribe>
-				{/each}
-			</tbody>
-		</table>
-	</section>
-
-	<nav>
-		<ol>
-			<li>
-				<button disabled={!$hasPreviousPage} on:click={() => $pageIndex--}>
-					&#10094; Previous
-				</button>
-			</li>
-			<li>Page {$pageIndex + 1} of {$pageCount}</li>
-			<li>
-				<button disabled={!$hasNextPage} on:click={() => $pageIndex++}> Next &#10095; </button>
-			</li>
-		</ol>
-	</nav>
+	<IndexTable data={collection} {label} {columns} {url} />
 </article>
-
-<style>
-	h2 {
-		max-inline-size: none;
-	}
-
-	table {
-		width: 100%;
-	}
-
-	nav ol {
-		display: flex;
-		justify-content: space-between;
-		width: 100%;
-	}
-
-	button {
-		font-size: var(--font-size-1);
-
-		&:disabled {
-			opacity: 0.6;
-			cursor: not-allowed;
-			box-shadow: none;
-
-			&:hover {
-				box-shadow: none;
-				color: unset;
-			}
-		}
-	}
-</style>
