19fafef0bbd326c8a90697d74d4b4318d111f063
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-18 14:59:27 +0100

    feat(indextable): add file with reusable column definitions

---
+++ src/lib/tableColumns.ts
@@ -0,0 +1,63 @@
+export const dateColumn = {
+	header: 'Date',
+	id: 'date',
+	accessor: (item: { start_date: Date; end_date: Date }) => ({
+		start_date: item.start_date,
+		end_date: item.end_date
+	}),
+	cell: ({ value }: { value: { start_date: Date; end_date: Date } }) => {
+		const { start_date, end_date } = value;
+		return start_date !== end_date ? `${start_date}–${end_date}` : start_date;
+	},
+	plugins: {
+		sort: {
+			getSortValue(item: { start_date: Date; end_date: Date }) {
+				return [item.start_date, item.end_date];
+			}
+		}
+	}
+};
+
+export const nameColumn = {
+	header: 'Name',
+	id: 'name',
+	accessor: (item: { slug: string; name: string }) => ({ slug: item.slug, name: item.name }),
+	plugins: {
+		sort: {
+			getSortValue(item: { slug: string; name: string }) {
+				return item.slug;
+			}
+		}
+	}
+};
+
+export const locationColumn = {
+	header: 'Location',
+	accessor: 'location',
+	cell: ({ value }: { value: string }) =>
+		value
+			?.split('-')
+			.slice(0, -1)
+			.map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+			.join(' ') || 'N/A',
+	plugins: {
+		sort: {
+			getSortValue(item: string) {
+				return item || '';
+			}
+		}
+	}
+};
+
+export const statusColumn = {
+	header: 'Status',
+	accessor: 'draft',
+	cell: ({ value }: { value: boolean }) => (value ? 'draft' : 'edited'),
+	plugins: {
+		sort: {
+			getSortValue(item: boolean) {
+				return item ? 0 : 1;
+			}
+		}
+	}
+};
