2b96c8ee069eb41b465b3ab0c9eacf0f23300283
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-18 14:59:46 +0100

    feat(donations): use reusable column definitions

--- src/routes/database/donations/+page.svelte
+++ src/routes/database/donations/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as config from '$lib/config';
 	import IndexTable from '$lib/components/IndexTable.svelte';
+	import { dateColumn, nameColumn, locationColumn, statusColumn } from '$lib/tableColumns';
 
 	export let data;
 
@@ -8,75 +9,17 @@
 	const label = 'donations';
 
 	const columns = [
-		{
-			header: 'Date',
-			id: 'date',
-			accessor: (item: { start_date: string; end_date: string }) => ({
-				start_date: item.start_date,
-				end_date: item.end_date
-			}),
-			cell: ({ value }: { value: { start_date: string; end_date: string } }) => {
-				const { start_date, end_date } = value;
-				return start_date !== end_date ? `${start_date} - ${end_date}` : start_date;
-			},
-			plugins: {
-				sort: {
-					getSortValue(item: { start_date: string; end_date: string }) {
-						return [item.start_date, item.end_date];
-					}
-				}
-			}
-		},
-		{
-			header: 'Name',
-			id: 'name',
-			accessor: (item: { slug: string; name: string }) => ({ slug: item.slug, name: item.name }),
-			plugins: {
-				sort: {
-					getSortValue(item: { slug: string; name: string }) {
-						return item.slug;
-					}
-				}
-			}
-		},
-		{
-			header: 'Location',
-			accessor: 'location',
-			cell: ({ value }: { value: string }) =>
-				value
-					?.split('-')
-					.slice(0, -1)
-					.map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
-					.join(' ') || 'N/A',
-			plugins: {
-				sort: {
-					getSortValue(item: string) {
-						return item ?? '';
-					}
-				}
-			}
-		},
+		dateColumn,
+		nameColumn,
+		locationColumn,
 		{
 			header: 'Price',
 			accessor: 'price',
-			cell: ({ value }: { value: number }) => value.toLocaleString()
+			cell: ({ value, row }: { value: number; row: { original: { currency: string } } }) =>
+				`${value.toLocaleString()} ${row.original.currency}`
 		},
-		{
-			header: 'Currency',
-			accessor: 'currency'
-		},
-		{
-			header: 'Status',
-			accessor: 'draft',
-			cell: ({ value }: { value: boolean }) => (value ? 'draft' : 'edited'),
-			plugins: {
-				sort: {
-					getSortValue(item: boolean) {
-						return item ? 0 : 1;
-					}
-				}
-			}
-		}
+		{ header: 'Currency', accessor: 'currency' },
+		statusColumn
 	];
 </script>
 
