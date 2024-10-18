d2e8f3e153ebb4894648797e3d7fc9680b2251b6
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-18 14:34:14 +0100

    feat(donations): display currency in a separate column and add status column

--- src/routes/database/donations/+page.svelte
+++ src/routes/database/donations/+page.svelte
@@ -11,14 +11,17 @@
 		{
 			header: 'Date',
 			id: 'date',
-			accessor: (item) => ({ start_date: item.start_date, end_date: item.end_date }),
-			cell: ({ value }) => {
+			accessor: (item: { start_date: string; end_date: string }) => ({
+				start_date: item.start_date,
+				end_date: item.end_date
+			}),
+			cell: ({ value }: { value: { start_date: string; end_date: string } }) => {
 				const { start_date, end_date } = value;
 				return start_date !== end_date ? `${start_date} - ${end_date}` : start_date;
 			},
 			plugins: {
 				sort: {
-					getSortValue(item) {
+					getSortValue(item: { start_date: string; end_date: string }) {
 						return [item.start_date, item.end_date];
 					}
 				}
@@ -27,7 +30,7 @@
 		{
 			header: 'Name',
 			id: 'name',
-			accessor: (item) => ({ slug: item.slug, name: item.name }),
+			accessor: (item: { slug: string; name: string }) => ({ slug: item.slug, name: item.name }),
 			plugins: {
 				sort: {
 					getSortValue(item: { slug: string; name: string }) {
@@ -39,7 +42,7 @@
 		{
 			header: 'Location',
 			accessor: 'location',
-			cell: ({ value }) =>
+			cell: ({ value }: { value: string }) =>
 				value
 					?.split('-')
 					.slice(0, -1)
@@ -56,7 +59,23 @@
 		{
 			header: 'Price',
 			accessor: 'price',
-			cell: ({ value, row }) => `${value.toLocaleString()} ${row.original.currency}`
+			cell: ({ value }: { value: number }) => value.toLocaleString()
+		},
+		{
+			header: 'Currency',
+			accessor: 'currency'
+		},
+		{
+			header: 'Status',
+			accessor: 'draft',
+			cell: ({ value }: { value: boolean }) => (value ? 'draft' : 'edited'),
+			plugins: {
+				sort: {
+					getSortValue(item: boolean) {
+						return item ? 0 : 1;
+					}
+				}
+			}
 		}
 	];
 </script>
