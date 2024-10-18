a5bc04a251b794ec1043719c50604a323e92eebc
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-18 16:16:36 +0100

    feat(people): add index table to render people

--- src/routes/database/people/+page.svelte
+++ src/routes/database/people/+page.svelte
@@ -1,17 +1,77 @@
 <script lang="ts">
-  export let data;
+	import * as config from '$lib/config';
+	import IndexTable from '$lib/components/IndexTable.svelte';
+	import { nameColumn, statusColumn } from '$lib/tableColumns';
+
+	export let data;
+
+	const label = 'people';
+	const { people, url } = data;
+
+	const columns = [
+		{ header: 'Slug', accessor: 'slug' },
+		nameColumn,
+		{
+			header: 'Gender',
+			accessor: 'gender',
+			cell: ({ value }: { value: string }) => value ?? 'N/A',
+			plugins: {
+				sort: {
+					getSortValue(item: string) {
+						return item || '';
+					}
+				}
+			}
+		},
+		{
+			header: 'Nationality',
+			accessor: 'nationality',
+			cell: ({ value }: { value: string }) => value ?? 'N/A',
+			plugins: {
+				sort: {
+					getSortValue(item: string) {
+						return item || '';
+					}
+				}
+			}
+		},
+		{
+			header: 'Ethnicity',
+			accessor: 'ethnicity',
+			cell: ({ value }: { value: string }) => value ?? 'N/A',
+			plugins: {
+				sort: {
+					getSortValue(item: string) {
+						return item || '';
+					}
+				}
+			}
+		},
+		{
+			header: 'Language',
+			accessor: 'language',
+			cell: ({ value }: { value: string }) => value ?? 'N/A',
+			plugins: {
+				sort: {
+					getSortValue(item: string) {
+						return item || '';
+					}
+				}
+			}
+		},
+		statusColumn
+	];
+	const sortBy = { initialSortKeys: [{ id: 'name', order: 'asc' }] };
 </script>
 
-<article>
-  <header>
-    <h1>People</h1>
-  </header>
+<svelte:head>
+	<title>People | {config.title}</title>
+</svelte:head>
 
-  <section>
-    <ul>
-      {#each data.people as person}
-        <li><a href="{data.url}/{person.slug}">{person.name}</a></li>
-      {/each}
-    </ul>
-  </section>
+<article>
+	<header>
+		<h1>People</h1>
+	</header>
+
+	<IndexTable data={people} {label} {columns} {sortBy} {url} />
 </article>
