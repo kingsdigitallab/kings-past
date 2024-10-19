d7a040e16fd8c288c0888a96e9fe74705ab1d35d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-19 08:59:56 +0100

    feat(places): render places on map

--- src/routes/database/places/+page.svelte
+++ src/routes/database/places/+page.svelte
@@ -1,17 +1,60 @@
 <script lang="ts">
-  export let data;
+	import * as config from '$lib/config';
+	import IndexTable from '$lib/components/IndexTable.svelte';
+	import PlacesMap from '$lib/components/PlacesMap.svelte';
+	import { nameColumn, statusColumn } from '$lib/tableColumns';
+
+	export let data;
+
+	const label = 'places';
+	const { collection, url } = data;
+
+	const columns = [
+		nameColumn,
+		{
+			header: 'Latitude',
+			accessor: 'latitude',
+			cell: ({ value }: { value: number }) => (value ? value.toFixed(2) : 'N/A'),
+			plugins: {
+				sort: {
+					getSortValue(item: number) {
+						return item || 0;
+					}
+				}
+			}
+		},
+		{
+			header: 'Longitude',
+			accessor: 'longitude',
+			cell: ({ value }: { value: number }) => (value ? value.toFixed(2) : 'N/A'),
+			plugins: {
+				sort: {
+					getSortValue(item: number) {
+						return item || 0;
+					}
+				}
+			}
+		},
+		statusColumn
+	];
+	const sortBy = { initialSortKeys: [{ id: 'name', order: 'asc' }] };
 </script>
 
-<article>
-  <header>
-    <h1>Places</h1>
-  </header>
+<svelte:head>
+	<title>Places | {config.title}</title>
+</svelte:head>
 
-  <section>
-    <ul>
-      {#each data.collection as item}
-        <li><a href="{data.url}/{item.slug}">{item.name}</a></li>
-      {/each}
-    </ul>
-  </section>
+<article>
+	<header>
+		<h1>Places</h1>
+	</header>
+
+	<section>
+		<h2>Displaying {collection.length} places</h2>
+		<PlacesMap places={collection} />
+	</section>
+
+	<section>
+		<IndexTable data={collection} {label} {columns} {sortBy} {url} />
+	</section>
 </article>
