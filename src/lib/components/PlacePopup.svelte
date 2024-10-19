e7ae0a9740fa6e19f4da3699bb94fc7238c021dd
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-19 08:59:34 +0100

    feat(places): add map component

---
+++ src/lib/components/PlacePopup.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Place } from '$lib/types';
+
+	export let place: Place;
+</script>
+
+<div class="place-popup">
+	<h3><a href="/database/places/{place.slug}">{place.name}</a></h3>
+	<p><strong>Status:</strong> {place.draft ? 'draft' : 'edited'}</p>
+	<p>{place.description || 'No description available.'}</p>
+</div>
+
+<style>
+	:global(.maplibregl-popup-content button) {
+		color: var(--text-1);
+
+		&:hover {
+			box-shadow: var(--shadow-2);
+		}
+	}
+
+	:global(.maplibregl-popup-content button:hover) {
+		box-shadow: var(--shadow-2);
+	}
+</style>
