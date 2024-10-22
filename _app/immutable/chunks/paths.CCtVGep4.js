4c970ff2cc90a0284212435f6174063a817d5fd1
jmiguelv <jmiguelv@users.noreply.github.com>
2024-10-22 15:12:17 +0000

    deploy: ef99e4964900b4ecc43645a5235f81671bed0235

---
+++ _app/immutable/chunks/paths.CCtVGep4.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_vsv3td)==null?void 0:s.base)??"/kings-past";var t;const e=((t=globalThis.__sveltekit_vsv3td)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/entry/start.C6JUyxPz.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DMTI79DL.js";export{t as start};
