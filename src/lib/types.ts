c7671eb51a390fe537debcb8a53ccfb724fee911
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-23 12:58:12 +0100

    chore(types): add types for related objects

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -21,8 +21,11 @@ export type TableNames = keyof Database['public']['Tables'];
 export type Donation = Tables<'donation'>;
 export type Event = Tables<'event'>;
 export type Organisation = Tables<'organisation'>;
+export type OrganisationSource = Tables<'organisation_source'>;
 export type Person = Tables<'person'>;
 export type PersonMoment = Tables<'person_moment'>;
+export type PersonMemberOf = Tables<'person_member_of'>;
 export type Place = Tables<'place'>;
+export type Source = Tables<'source'>;
 
 export type KPRecord = Donation | Event | Organisation | Person | Place;
