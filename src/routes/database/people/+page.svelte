e3e9fba001273c48041a1c9497e956eb3f33107a
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-24 07:52:28 +0100

    fix(people): disable card view for now, wip

--- src/routes/database/people/+page.svelte
+++ src/routes/database/people/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	import * as config from '$lib/config';
 	import IndexTable from '$lib/components/IndexTable.svelte';
-	import IndexCards from '$lib/components/IndexCards.svelte';
+	// import IndexCards from '$lib/components/IndexCards.svelte';
 	import { nameColumn, statusColumn } from '$lib/tableColumns';
-	import { LucideTable, LucideLayoutGrid } from 'lucide-svelte';
+	// import { LucideTable, LucideLayoutGrid } from 'lucide-svelte';
 
 	export let data;
 
 	const label = 'people';
 	const { people, url } = data;
 
-	let viewMode: 'table' | 'cards' = 'table';
+	// let viewMode: 'table' | 'cards' = 'table';
 
 	const columns = [
 		{ header: 'Slug', accessor: 'slug' },
@@ -99,11 +99,11 @@
 
 	<!-- {#if viewMode === 'table'} -->
 	<IndexTable data={people} {label} {columns} {sortBy} {url} />
-	<!-- {:else}
+	<!-- {:else}‸
 		<IndexCards data={people} {columns} {url} />
 	{/if} -->
 </article>
-
+<!-- 
 <style>
 	.view-toggle {
 		margin-bottom: 1rem;
@@ -122,4 +122,4 @@
 		background-color: #007bff;
 		color: white;
 	}
-</style>
+</style> -->
