58373c3cdb3806cc16cced5e13bffbb3612fd480
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-25 15:06:29 +0100

    feat(people): add test card view, not styled

---
+++ src/lib/components/IndexCards.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import type { Column } from '$lib/types';
+
+	export let data: any[];
+	export let url: string;
+	export let columns: Column[];
+
+	function formatValue(item: any, column: Column) {
+		if (column.cell) {
+			return column.cell({ value: item[column.accessor] });
+		}
+		return item[column.accessor] ?? 'N/A';
+	}
+</script>
+
+<div class="cards-container">
+	{#each data as item}
+		<article class="card">
+			<header>
+				<h2><a href={`${url}/${item.slug}`}>{item.name}</a></h2>
+			</header>
+			<dl>
+				{#each columns as column}
+					{#if column.header !== 'Name' && column.header !== 'Slug'}
+						<div class="dl-item">
+							<dt>{column.header}</dt>
+							<dd>{formatValue(item, column)}</dd>
+						</div>
+					{/if}
+				{/each}
+			</dl>
+		</article>
+	{/each}
+</div>
+
+<style>
+	.cards-container {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+		gap: var(--size-4);
+	}
+
+	.card {
+		background-color: #f0f0f0;
+		border-radius: var(--radius-2);
+		padding: var(--size-4);
+		transition: box-shadow 0.3s ease;
+	}
+
+	.card:hover {
+		box-shadow: var(--shadow-3);
+	}
+
+	.card h2 {
+		font-size: var(--font-size-4);
+		margin-top: 0;
+		margin-bottom: var(--size-3);
+	}
+
+	dl {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: var(--size-2) var(--size-4);
+		margin: 0;
+	}
+
+	.dl-item {
+		break-inside: avoid;
+		page-break-inside: avoid;
+	}
+
+	dt {
+		font-weight: bold;
+		font-size: var(--font-size-1);
+		color: var(--color-text-muted);
+	}
+
+	dd {
+		margin-left: 0;
+		margin-bottom: var(--size-1);
+		font-size: var(--font-size-2);
+	}
+</style>
--- src/routes/database/people/+page.svelte
+++ src/routes/database/people/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	import * as config from '$lib/config';
 	import IndexTable from '$lib/components/IndexTable.svelte';
-	// import IndexCards from '$lib/components/IndexCards.svelte';
+	import IndexCards from '$lib/components/IndexCards.svelte';
 	import { nameColumn, alternativeNamesColumn, statusColumn } from '$lib/tableColumns';
-	// import { LucideTable, LucideLayoutGrid } from 'lucide-svelte';
+	import { LucideTable, LucideLayoutGrid } from 'lucide-svelte';
 
 	export let data;
 
 	const label = 'people';
 	const { people, url } = data;
 
-	// let viewMode: 'table' | 'cards' = 'table';
+	let viewMode: 'table' | 'cards' = 'table';
 
 	const columns = [
 		{ header: 'Slug', accessor: 'slug' },
@@ -76,23 +76,23 @@
 <article>
 	<header>
 		<h1>People</h1>
-		<!-- <div class="view-toggle">
+		<div class="view-toggle">
 			<button class:active={viewMode === 'table'} on:click={() => (viewMode = 'table')}
 				><LucideTable />Table view</button
 			>
 			<button class:active={viewMode === 'cards'} on:click={() => (viewMode = 'cards')}
 				><LucideLayoutGrid />Card view</button
 			>
-		</div> -->
+		</div>
 	</header>
 
-	<!-- {#if viewMode === 'table'} -->
-	<IndexTable data={people} {label} {columns} {sortBy} {url} />
-	<!-- {:else}‸
+	{#if viewMode === 'table'}
+		<IndexTable data={people} {label} {columns} {sortBy} {url} />
+	{:else}
 		<IndexCards data={people} {columns} {url} />
-	{/if} -->
+	{/if}
 </article>
-<!-- 
+
 <style>
 	.view-toggle {
 		margin-bottom: 1rem;
@@ -111,4 +111,4 @@
 		background-color: #007bff;
 		color: white;
 	}
-</style> -->
+</style>
