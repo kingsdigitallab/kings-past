bae2f9d9f842f2a83c134181821366a3766562c7
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-28 16:33:09 +0000

    feat(layout): add global title to head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,24 @@
 	import Footer from '$lib/components/footer.svelte';
 	import Header from '$lib/components/header.svelte';
 	import PageTransition from '$lib/components/transition.svelte';
+	import * as config from '$lib/config';
 
 	import 'open-props/style';
 	import 'open-props/normalize';
 
 	import '$css/app.css';
+	import { page } from '$app/stores';
 
 	export let data;
 </script>
+
+<svelte:head>
+	{#if $page.data.title}
+		<title>{config.title} | {$page.data.title}</title>
+	{:else}
+		<title>{config.title}</title>
+	{/if}
+</svelte:head>
 
 <div class="layout surface-1">
 	<Header />
