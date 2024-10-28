2ff10a8bfebe889c8006914b61662612a14f49aa
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-28 22:04:28 +0000

    feat(donations): add component to render donations

---
+++ src/lib/components/DonationsList.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { Donation } from '$lib/types';
+
+	export let entityName: string;
+	export let donationsAsAgent: Donation[] | undefined;
+	export let donationsAsRecipient: Donation[] | undefined;
+</script>
+
+{#if donationsAsAgent?.length || donationsAsRecipient?.length}
+	<section>
+		<h2>Donations</h2>
+		{#if donationsAsAgent?.length}
+			<section>
+				<h3>Made by {entityName}</h3>
+				<ul>
+					{#each donationsAsAgent as donation}
+						<li>
+							<a href="../donations/{donation.slug}">{donation.name}</a>
+						</li>
+					{/each}
+				</ul>
+			</section>
+		{/if}
+		{#if donationsAsRecipient?.length}
+			<section>
+				<h3>Received by {entityName}</h3>
+				<ul>
+					{#each donationsAsRecipient as donation}
+						<li>
+							<a href="../donations/{donation.slug}">{donation.name}</a>
+						</li>
+					{/each}
+				</ul>
+			</section>
+		{/if}
+	</section>
+{/if}
