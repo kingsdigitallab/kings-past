539fc4203e54185abdfa927d5fa0746ea109a402
jmiguelv <jmiguelv@users.noreply.github.com>
2024-10-29 21:53:29 +0000

    deploy: 79f1612033180446931bc3c3599cfcf777842517

--- _app/immutable/chunks/paths.BP6JKaBO.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_10to313)==null?void 0:s.base)??"/kings-past";var t;const e=((t=globalThis.__sveltekit_10to313)==null?void 0:t.assets)??a;export{e as a,a as b};
