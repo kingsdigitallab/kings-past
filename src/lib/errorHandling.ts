adff20bf1655ab567c852066bffcafed534bd931
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-29 07:23:14 +0000

    refactor(errorhandling): log to the console on dev

--- src/lib/errorHandling.ts
+++ src/lib/errorHandling.ts
@@ -1,6 +1,11 @@
+import { dev } from '$app/environment';
 import { error } from '@sveltejs/kit';
 
 export function handleLoadError(slug: string, e: unknown): never {
+	if (dev) {
+		console.error(e);
+	}
+
 	if (e instanceof Error) {
 		throw error(404, `Could not load ${slug}: ${e.message}`);
 	} else {
