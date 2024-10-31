dac945bb7800685a5128fad977787dd2c39b7647
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-31 14:52:32 +0000

    feat(search): enable history

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import { afterNavigate, pushState } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
 	import { PagefindUI } from '@pagefind/default-ui';
 	import { onMount } from 'svelte';
 
 	import '@pagefind/default-ui/css/ui.css';
 
+	let pagefind: PagefindUI;
+
 	onMount(() => {
 		if (browser) {
-			new PagefindUI({
+			pagefind = new PagefindUI({
 				element: '#search',
 				autofocus: true,
 				bundlePath: `${base}/pagefind/`,
 				showEmptyFilters: false
 			});
+
+			const searchInput = document.querySelector('#search input') as HTMLInputElement;
+			if (searchInput) {
+				searchInput.addEventListener('input', (event: Event) => {
+					const target = event.target as HTMLInputElement;
+					const url = new URL(window.location.href);
+					url.searchParams.set('q', target.value);
+					pushState(url, '');
+				});
+			}
+
+			pagefind.triggerSearch($page.url.searchParams.get('q'));
 		}
+	});
+
+	afterNavigate(() => {
+		pagefind?.triggerSearch($page.url.searchParams.get('q'));
 	});
 </script>
 
