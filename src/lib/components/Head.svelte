d2d4025a6e78531e38ea0e186c5366fe9ea931dd
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-11-01 18:25:57 +0000

    feat(head): add component to load meta tags

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+	import { page } from '$app/stores';
+
+	export let metadata;
+</script>
+
+{#if metadata?.title}
+	<title>{config.title} | {metadata.title}</title>
+{:else}
+	<title>{config.title}</title>
+{/if}
+<meta name="description" content={metadata?.excerpt || config.description} />
+{#if metadata?.tags}
+	<meta name="keywords" content={metadata.tags} />
+{/if}
+<link rel="canonical" href={$page.url.toString()} />
