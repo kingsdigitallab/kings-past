e7785e7c4de5f2648bcdc7da16818aaefac23fb4
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-11-01 08:04:57 +0000

    feat(moments): style the people list using a responsive grid

--- src/routes/moments/[slug]/+page.svelte
+++ src/routes/moments/[slug]/+page.svelte
@@ -11,12 +11,14 @@
 			<h2>Detailed research</h2>
 
 			{#each Object.entries(essaysByCategory) as [category, essays]}
-				<h3>{category.charAt(0).toUpperCase() + category.slice(1)}</h3>
-				<ul>
-					{#each essays as essay}
-						<li><a href="{slug}/{essay.slug}">{essay.title}</a></li>
-					{/each}
-				</ul>
+				<section>
+					<h3>{category.charAt(0).toUpperCase() + category.slice(1)}</h3>
+					<ul>
+						{#each essays as essay}
+							<li><a href="{slug}/{essay.slug}">{essay.title}</a></li>
+						{/each}
+					</ul>
+				</section>
 			{/each}
 		</section>
 	{/if}
@@ -46,6 +48,47 @@
 		padding-inline: var(--size-4);
 		padding: 0;
 
+		& h2 {
+			background-color: var(--powder-blue);
+			font-size: var(--font-size-5);
+			font-weight: 200;
+			margin: 0;
+			min-width: 100%;
+			padding: var(--size-3) var(--size-4);
+		}
+
+		& section {
+			padding-left: var(--size-4);
+		}
+
+		& ul {
+			list-style-type: none;
+			margin-left: var(--size-000);
+			padding-left: var(--size-5);
+		}
+
+		& ul li {
+			break-inside: avoid;
+			font-size: var(--font-size-3);
+			margin-block: var(--size-2);
+			padding-inline-start: var(--size-2);
+
+			& a {
+				border-left: var(--size-2) solid var(--surface-1);
+				display: block;
+				padding-left: var(--size-3);
+				text-decoration: none;
+				width: calc(100% - var(--size-2));
+
+				&:hover {
+					background-color: var(--midnight-blue);
+					border-left: var(--size-2) solid var(--yellow);
+					color: white;
+					opacity: 1;
+				}
+			}
+		}
+
 		& details {
 			background-color: inherit;
 
@@ -57,39 +100,12 @@
 			}
 		}
 
-		& h2 {
-			background-color: var(--powder-blue);
-			font-size: var(--font-size-5);
-			font-weight: 200;
-			margin: 0;
-			min-width: 100%;
-			padding: var(--size-3) var(--size-4);
-		}
+		& details ul {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
 
-		& ul {
-			list-style-type: none;
-			margin-left: -10px;
-			padding-left: var(--size-5);
-
-			& li {
-				font-size: var(--font-size-3);
-				margin-block: var(--size-2);
-				padding-inline-start: var(--size-2);
-
-				& a {
-					border-left: var(--size-2) solid var(--surface-1);
-					display: block;
-					padding-left: var(--size-3);
-					text-decoration: none;
-					width: calc(100% - var(--size-2));
-
-					&:hover {
-						background-color: var(--midnight-blue);
-						border-left: var(--size-2) solid var(--yellow);
-						color: white;
-						opacity: 1;
-					}
-				}
+			@media (max-width: 768px) {
+				grid-template-columns: 1fr;
 			}
 		}
 	}
