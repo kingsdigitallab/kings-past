9c879fe90b37245be734aec5cb2f9e8fdd47abbc
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-11-05 16:05:50 +0000

    feat(layout): add analytics via goatcounter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import { page } from '$app/stores';
 	import Breadcrumbs from '$lib/components/Breadcrumbs.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Head from '$lib/components/Head.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import PageTransition from '$lib/components/Transition.svelte';
+	import { onMount } from 'svelte';
 
 	import 'open-props/style';
 	import 'open-props/normalize';
@@ -12,6 +14,32 @@
 	import '$css/app.css';
 
 	export let data;
+
+	function shouldTrack() {
+		if (navigator.doNotTrack === '1' || navigator.doNotTrack === 'yes') {
+			return false;
+		}
+
+		return true;
+	}
+
+	function initGoatCounter() {
+		if (!shouldTrack()) return;
+
+		const script = document.createElement('script');
+		script.dataset.goatcounter = 'https://kingspast.goatcounter.com/count';
+		script.src = '//gc.zgo.at/count.js';
+		script.async = true;
+		script.onerror = () => console.warn('Failed to load GoatCounter');
+
+		document.body.appendChild(script);
+	}
+
+	onMount(() => {
+		if (!dev && shouldTrack()) {
+			initGoatCounter();
+		}
+	});
 
 	$: metadata = $page.data?._metadata || {};
 </script>
