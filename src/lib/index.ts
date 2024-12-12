195e8183d1151e3890a162e1cb0236681fc8839b
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-12-12 11:45:06 +0000

    fix(index): pass time when creating dates
    To prevent dates from changing across different environments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,10 +6,10 @@ export function formatDonationDate(donation: Donation) {
 
 export function formatDate(startDate: string, endDate: string, locale = 'en-GB') {
 	if (startDate === endDate) {
-		return new Intl.DateTimeFormat(locale).format(new Date(startDate));
+		return new Intl.DateTimeFormat(locale).format(new Date(`${startDate}T12:00:00Z`));
 	}
 
-	return `${new Intl.DateTimeFormat(locale).format(new Date(startDate))}–${new Intl.DateTimeFormat(locale).format(new Date(endDate))}`;
+	return `${new Intl.DateTimeFormat(locale).format(new Date(`${startDate}T12:00:00Z`))}–${new Intl.DateTimeFormat(locale).format(new Date(`${endDate}T12:00:00Z`))}`;
 }
 
 export function formatEventDate(event: Event) {
