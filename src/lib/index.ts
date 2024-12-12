7e5a1aaeae2a6e3e7f1648c2b46867a23156ddca
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-12-12 07:16:28 +0000

    fix(index): set locale of format date
    To prevent dates from changing across different environments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,10 +4,12 @@ export function formatDonationDate(donation: Donation) {
 	return formatDate(donation?.start_date, donation?.end_date);
 }
 
-export function formatDate(startDate: string, endDate: string) {
-	return startDate !== endDate
-		? `${new Date(startDate).toLocaleDateString()}–${new Date(endDate).toLocaleDateString()}`
-		: new Date(startDate).toLocaleDateString();
+export function formatDate(startDate: string, endDate: string, locale = 'en-GB') {
+	if (startDate === endDate) {
+		return new Intl.DateTimeFormat(locale).format(new Date(startDate));
+	}
+
+	return `${new Intl.DateTimeFormat(locale).format(new Date(startDate))}–${new Intl.DateTimeFormat(locale).format(new Date(endDate))}`;
 }
 
 export function formatEventDate(event: Event) {
