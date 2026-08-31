b117222869ee823c53c40937525965c72a67d91c
jmiguelv <jmiguelv@users.noreply.github.com>
2026-08-31 11:10:59 +0000

    deploy: d9939557d294441fac0cd6ce60150972ddf1ed84

---
+++ _app/immutable/chunks/stores.D8GlbmWw.js
@@ -0,0 +1,1 @@
+import{s as e}from"./entry.DEW4M-kp.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.BLzV6PSA.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.DEW4M-kp.js";export{a as start};
